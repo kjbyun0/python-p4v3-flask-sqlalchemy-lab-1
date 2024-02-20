bede062a62d4317ecb24cc0982d3869c46bd4dd3
KI JONG BYUN <kjbyun0@gmail.com>
2024-02-20 15:38:51 -0600

    Assignment Done

--- server/app.py
+++ server/app.py
@@ -21,7 +21,25 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def get_by_id(id):
+    eq = Earthquake.query.filter_by(id=id).first()
+    if eq:
+        body = eq.to_dict()
+        status = 200
+    else:
+        body = {'message': f'Earthquake {id} not found.'}
+        status = 404
+    return make_response(body, status)
 
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_by_magnitude(magnitude):
+    eqs = [eq.to_dict() for eq in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()]
+    body = {
+        'count': len(eqs),
+        'quakes': eqs
+    }
+    return make_response(body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
